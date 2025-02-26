f3276be368037f066b2848ce3e69a664daf47c9a
David McCullough <dmccullough488@gmail.com>
2025-02-26 00:33:26 -0600

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import Header from "./Header"
-import Main from "./main"
+//import Main from "./Main"
 
 export default function App() {
   return (
     <>
       <Header />
-      <Main />
+
     </>
   )
 }
