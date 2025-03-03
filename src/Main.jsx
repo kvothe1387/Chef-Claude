2c899119fa49e3abe750cec63d79238b142726b9
David McCullough <dmccullough488@gmail.com>
2025-03-03 00:32:42 -0600

    updated ingredient list conditional rendering

--- src/Main.jsx
+++ src/Main.jsx
@@ -4,15 +4,20 @@ export default function Main() {
 
   const [ingredients, setIngredients] = React.useState([])
 
-  function handleFormAction(formData) {
-    const newIngredient = formData.get("ingredient")
-    setIngredients(prevIngredients => [...prevIngredients, newIngredient])
-  }
+  const ingredientsListItems = ingredients.map(ingredient => (
+    <li key={ingredient}>{ingredient}</li>
+  ))
 
+  function addIngredient(formData) {
+    const newIngredient = formData.get("ingredient")
+    if (newIngredient) {
+      setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+    }
+  }
 
   return (
     <main>
-      <form onSubmit={handleFormAction} className="add-ingredient-form">
+      <form action={addIngredient} className="add-ingredient-form">
         <input
           type="text"
           placeholder="e.g. oregano"
@@ -21,11 +26,25 @@ export default function Main() {
         />
         <button>Add ingredient</button>
       </form>
-      <ul>
-        {ingredients.map(ingredient => (
-          <li key={ingredient}>{ingredient}</li>
-        ))}
-      </ul>
+
+      {ingredients.length > 0 && (
+        <section>
+          <h2>Ingredients on hand:</h2>
+          <ul className="ingredients-list" aria-live="polite">
+            {ingredientsListItems}
+          </ul>
+
+          {ingredients.length > 3 && (
+            <div className="get-recipe-container">
+              <div>
+                <h3>Ready for a recipe?</h3>
+                <p>Generate a recipe from your list of ingredients.</p>
+              </div>
+              <button>Get a recipe</button>
+            </div>
+          )}
+        </section>
+      )}
     </main>
   )
 }
